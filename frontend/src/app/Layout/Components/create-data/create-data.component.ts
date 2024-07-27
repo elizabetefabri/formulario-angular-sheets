789a5a1a088418b7caaf4c6ce87d278904b3cb4e
Elizabete Fabri <elizabete958@gmail.com>
2024-07-27 01:16:46 -0300

    Create: componente criado para efetuar o login com sucesso e sem erros.

---
+++ frontend/src/app/Layout/Components/create-data/create-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-data',
+  templateUrl: './create-data.component.html',
+  styleUrls: ['./create-data.component.css']
+})
+export class CreateDataComponent {
+
+}
