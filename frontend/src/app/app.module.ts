0d8733ed1ab70dc1103472cb40eb6fde7c437630
Elizabete Fabri <elizabete958@gmail.com>
2024-07-27 01:19:44 -0300

    Create: modulos e componentes.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Layout/Components/login/login.component';
+import { AppModuleModule } from './Shared/AppModule/app-module.module';
+import { ErrorDialogComponent } from './Shared/Components/error-dialog/error-dialog.component';
+import { CreateDataComponent } from './Layout/Components/create-data/create-data.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ErrorDialogComponent,
+    CreateDataComponent
   ],
   imports: [
+    AppModuleModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
