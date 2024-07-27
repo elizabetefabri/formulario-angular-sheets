1a1ab133c512632c65543fbb764901725adb946f
Elizabete Fabri <elizabete958@gmail.com>
2024-07-27 01:19:04 -0300

    add as rotas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Layout/Components/login/login.component';
+import { CreateDataComponent } from './Layout/Components/create-data/create-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'formulario', component: CreateDataComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
