7f9c4f7cec8c90ab488c3a0ee6feb09952026024
Elizabete Fabri <elizabete958@gmail.com>
2024-07-27 01:21:17 -0300

    Create: novo modulo para importar os materials de Angular

---
+++ frontend/src/app/Shared/AppModule/app-module.module.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatCardModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppModuleModule { }
