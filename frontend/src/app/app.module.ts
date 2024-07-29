37c4abb6ebba52ac2a475d256f7a9dcfb478fcbd
Elizabete Fabri <elizabete958@gmail.com>
2024-07-28 23:12:50 -0300

    Refactor: atualização dos path

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,6 +11,17 @@ import { AppModuleModule } from './Shared/AppModule/app-module.module';
 import { ErrorDialogComponent } from './Shared/Components/error-dialog/error-dialog.component';
 import { CreateDataComponent } from './Layout/Components/create-data/create-data.component';
 import { HeaderComponent } from './Layout/Components/header/header.component';
+import { Step01CadastroCandidatoComponent } from './Layout/Components/create-data/step01-cadastro-candidato/step01-cadastro-candidato.component';
+import { SelectEstadoComponent } from './Layout/Components/create-data/step01-cadastro-candidato/components/select-estado/select-estado.component';
+import { InputTelefoneComponent } from './Layout/Components/create-data/step01-cadastro-candidato/components/input-telefone/input-telefone.component';
+import { InputCpfComponent } from './Layout/Components/create-data/step01-cadastro-candidato/components/input-cpf/input-cpf.component';
+import { InputRgComponent } from './Layout/Components/create-data/step01-cadastro-candidato/components/input-rg/input-rg.component';
+import { InputTituloEleitoralComponent } from './Layout/Components/create-data/step01-cadastro-candidato/components/input-titulo-eleitoral/input-titulo-eleitoral.component';
+import { SelectRacaComponent } from './Layout/Components/create-data/step01-cadastro-candidato/components/select-raca/select-raca.component';
+import { SelectEstadoCivilComponent } from './Layout/Components/create-data/step01-cadastro-candidato/components/select-estado-civil/select-estado-civil.component';
+import { SelectGrauInstrucaoComponent } from './Layout/Components/create-data/step01-cadastro-candidato/components/select-grau-instrucao/select-grau-instrucao.component';
+import { SelectOcupacaoComponent } from './Layout/Components/create-data/step01-cadastro-candidato/components/select-ocupacao/select-ocupacao.component';
+import { InputTelefone2Component } from './Layout/Components/create-data/step01-cadastro-candidato/components/input-telefone2/input-telefone2.component';
 
 
 @NgModule({
@@ -19,7 +30,18 @@ import { HeaderComponent } from './Layout/Components/header/header.component';
     LoginComponent,
     ErrorDialogComponent,
     CreateDataComponent,
-    HeaderComponent
+    HeaderComponent,
+    Step01CadastroCandidatoComponent,
+    SelectEstadoComponent,
+    InputTelefoneComponent,
+    InputCpfComponent,
+    InputRgComponent,
+    InputTituloEleitoralComponent,
+    SelectRacaComponent,
+    SelectEstadoCivilComponent,
+    SelectGrauInstrucaoComponent,
+    SelectOcupacaoComponent,
+    InputTelefone2Component
   ],
   imports: [
     AppModuleModule,
